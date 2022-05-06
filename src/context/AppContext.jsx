c3bdbdc3cf52f2a1e5a9444eea35aba9f2a7472f
Lane Le Prevost-Smith <lanelps@gmail.com>
2022-05-06 16:02:01 +1200

    Initial commit

---
+++ src/context/AppContext.jsx
@@ -0,0 +1,55 @@
+/* eslint-disable camelcase */
+import React, { createContext, useEffect, useState, useMemo } from "react";
+import PropTypes from "prop-types";
+import { globalHistory } from "@reach/router";
+
+export const AppContext = createContext({});
+
+/** ============================================================================
+ * @context
+ * Global application data.
+ */
+const AppProvider = ({ children }) => {
+  // ---------------------------------------------------------------------------
+  // context / ref / state
+
+  const [pathname, setPathname] = useState(null);
+  const [menuActive, setMenuActive] = useState(false);
+
+  // ---------------------------------------------------------------------------
+  // methods
+
+  // ...
+
+  // ---------------------------------------------------------------------------
+  // lifecycle
+
+  useEffect(() => {
+    if (typeof window !== `undefined` && window?.location?.pathname) {
+      setPathname(window.location.pathname);
+    }
+
+    return globalHistory.listen(({ location }) => {
+      setPathname(location.pathname);
+    });
+  }, []);
+
+  // ---------------------------------------------------------------------------
+  // render
+
+  const providerProps = useMemo(() => ({
+    pathname,
+    menuActive,
+    setMenuActive
+  }));
+
+  return (
+    <AppContext.Provider value={providerProps}>{children}</AppContext.Provider>
+  );
+};
+
+AppProvider.propTypes = {
+  children: PropTypes.node.isRequired
+};
+
+export default AppProvider;
