85facdab38dfaaa255e7f5df330427f5708f05c6
AmandaJ17 <amanda.stacey17@gmail.com>
2022-05-31 17:01:48 +1200

    Edits to external links

--- src/components/Expand/index.jsx
+++ src/components/Expand/index.jsx
@@ -1,39 +1,76 @@
 import React from "react";
 import { PopOut, Carousel, Grid } from "~components";
+import styled from "@emotion/styled";
 import { css } from "@emotion/react";
+import { useStaticQuery, graphql } from "gatsby";
 
-const Expand = () => (
-  <div>
-    {/* <Carousel /> */}
-    <h1
-      css={css`
-        padding: 150px 50px;
-        background-color: orange;
-      `}
-    >
-      *CAROUSEL*
-    </h1>
-    <Grid>
-      <p
+const Expand = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allSanityProject {
+        nodes {
+          description
+          links {
+            label
+            url
+          }
+        }
+      }
+    }
+  `);
+
+  const ExternalLinks = styled.div`
+    margin: 24px 12px;
+    font-size: 10px;
+    line-height: 110%;
+    letter-spacing: 0.01em;
+    text-transform: uppercase;
+    font-weight: 600;
+  `;
+
+  const Container = styled.div`
+    display: flex;
+    margin: 12px 12px 12px 0px;
+    color: #595959;
+  `;
+
+  return (
+    <div>
+      {/* <Carousel /> */}
+      <h1
         css={css`
-          grid-column: 1 / span 3;
+          padding: 150px 50px;
+          background-color: orange;
         `}
       >
-        A Naarm-Based music and arts initative creating welcoming, surreal
-        spaces celebrating music and community. Love Project wishes to share its
-        passion for music through beautiful events and sharing performances of
-        local and international talent. Love Project branding encaptulates a
-        moment of serenity, encouraging its community to take a step back from
-        the hustle and bustle of the everyday and to be present in a single
-        moment. The graphic system implemented is versatile, reductive yet
-        recognisable to represent the bespoke, understated nature of the brand.
-      </p>
-    </Grid>
-    <h5>EXTERNAL LINKS</h5>
-    <h5>FIGMA SPRINT FILE</h5>
-    <h5>LOVE PROJECT INSTAGRAM</h5>
-    <PopOut />
-  </div>
-);
+        *CAROUSEL*
+      </h1>
+      <Grid>
+        <p
+          css={css`
+            grid-column: 1 / span 3;
+            margin-top: 29px;
+            font-family: "Neue Haas Grotesk Display Pro";
+            font-style: normal;
+            font-weight: 500;
+            font-size: 18px;
+            line-height: 110%;
+          `}
+        >
+          {data.allSanityProject.nodes[1].description}
+        </p>
+      </Grid>
+      <ExternalLinks>
+        <h5>EXTERNAL LINKS</h5>
+        <Container>
+          <a href="{data.allSanityProject.nodes[1].links[1].url}">
+            {data.allSanityProject.nodes[1].links[1].label}
+          </a>
+        </Container>
+      </ExternalLinks>
+      <PopOut />
+    </div>
+  );
+};
 
 export default Expand;
