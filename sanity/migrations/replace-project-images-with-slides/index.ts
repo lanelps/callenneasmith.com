254e5bbcd39b43106c606a410f4db503556685f6
Lane Le Prevost-Smith <lanelps@gmail.com>
2025-01-14 10:41:19 +1100

    feat: Transform project tags in migration to replace images with structured slide data

--- sanity/migrations/replace-project-images-with-slides/index.ts
+++ sanity/migrations/replace-project-images-with-slides/index.ts
@@ -1,15 +1,23 @@
-import { defineMigration, at, setIfMissing, unset } from "sanity/migrate";
+import { defineMigration, at, setIfMissing, unset, set } from "sanity/migrate";
 
 const from = "images";
 const to = "slides";
 
 export default defineMigration({
-  title: "Replace project images with slides",
+  title: "Transform project tags",
   documentTypes: ["project"],
 
   migrate: {
     document(doc, context) {
-      return [at(to, setIfMissing(doc[from])), at(from, unset())];
+      console.log(`DOC`);
+      if (doc.tags) {
+        return set(
+          doc.tags.map((tag) => ({
+            text: tag,
+            url: ""
+          }))
+        );
+      }
     }
   }
 });
