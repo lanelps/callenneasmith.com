4617d06c51ae5d5285783bbcf60de27cd8857d67
Lane Le Prevost-Smith <lanelps@gmail.com>
2025-09-03 19:57:37 +1000

    refactor: Simplify Footer component structure and improve styling

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,8 +1,5 @@
 import React from "react";
 import styled from "@emotion/styled";
-import { css } from "@emotion/react";
-
-import { Grid } from "~components";
 
 import { breakpoint } from "~utils/css";
 
@@ -10,27 +7,23 @@ const Footer = styled.div`
   position: relative;
   width: 100%;
 
-  padding: 1.5rem 0 0.75rem;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1.5rem;
 
-  background: var(--color-laser);
+  padding: 1.5rem 0.5rem 0.75rem;
+
+  background: var(--color-blue);
+  color: var(--color-white);
   text-transform: uppercase;
-
-  h5 {
-    grid-column: 1 / -1;
-    margin-bottom: 1.5rem;
-  }
 
   a:first-of-type {
     grid-column: span 2;
   }
 
   ${breakpoint(`tablet`)} {
-    padding: 3rem 0 0.75rem;
-
-    h5 {
-      grid-column: 1 / span 1;
-      margin-bottom: 0;
-    }
+    flex-wrap: nowrap;
+    padding: 3rem 0.5rem 0.5rem;
 
     a:first-of-type {
       grid-column: span 1;
@@ -40,28 +33,17 @@ const Footer = styled.div`
 
 const footer = ({ className, contact }) => (
   <Footer className={`${(className && `${className} `) || ``}caption`}>
-    <Grid>
-      <h5 className="caption">Callen Neasmith 2022 ©</h5>
-      {contact.map((item) => (
-        <a
-          key={`${item?._key}-footer`}
-          href={`${item?.url}`}
-          target="_blank"
-          rel="noopener noreferrer"
-          css={css`
-            color: var(--color-off-black);
-
-            :hover {
-              color: var(--color-rich-black);
-            }
-
-            transition: color 0.3s ease;
-          `}
-        >
-          {item?.label}
-        </a>
-      ))}
-    </Grid>
+    <h5 className="caption">Callen Neasmith 2022 ©</h5>
+    {contact.map((item) => (
+      <a
+        key={`${item?._key}-footer`}
+        href={`${item?.url}`}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {item?.label}
+      </a>
+    ))}
   </Footer>
 );
 
